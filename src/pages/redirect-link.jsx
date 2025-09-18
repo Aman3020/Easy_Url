743b50ff7e6141fbfeabcdf906cafa0a7c3ab78a
CodaHolic <aman03singh18@gmail.com>
2025-09-18 17:22:47 +0530

    fixed usestate import

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState} from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/use-fetch';
 import { getLongUrl } from '../db/apiUrls';
